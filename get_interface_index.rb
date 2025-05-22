fc824b077db685e54b9705ccf7b1942ce2830cb3
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-22 02:20:38 -0400

    Refactor get_interface_index method to simplify socket handling and return both socket and index

--- get_interface_index.rb
+++ get_interface_index.rb
@@ -8,13 +8,11 @@ SIOCGIFINDEX = 0x8933
 
 def get_interface_index(interface_name)
   socket = Socket.open(:PACKET, :RAW)
-  begin
-    ifreq = [interface_name].pack("a#{IFREQ_SIZE}")
+  ifreq = [interface_name].pack("a#{IFREQ_SIZE}")
 
-    socket.ioctl(SIOCGIFINDEX, ifreq)
+  socket.ioctl(SIOCGIFINDEX, ifreq)
 
-    ifreq[Socket::IFNAMSIZ, IFINDEX_SIZE]
-  ensure
-    socket.close
-  end
+  index = ifreq[Socket::IFNAMSIZ, IFINDEX_SIZE]
+
+  [socket, index]
 end
