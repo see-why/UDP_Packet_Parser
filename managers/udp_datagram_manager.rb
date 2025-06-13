3a92980c97ee011494738b2860c4c75da5f03ed6
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-06-13 00:31:54 -0400

    Fix class name typo and improve byte handling in UDPDatagramManager

--- managers/udp_datagram_manager.rb
+++ managers/udp_datagram_manager.rb
@@ -1,31 +1,33 @@
 # frozen_string_literal: true
 
-class UDPDatagramMnager
+class UDPDatagramManager
+  attr_reader :bytes
+
   def initialize(bytes)
     @bytes = bytes
   end
 
   def source_port
-    word16(bytes[0], bytes[1])
+    word16(bytes[0].ord, bytes[1].ord)
   end
 
   def destination_port
-    word16(bytes[2], bytes[3])
+    word16(bytes[2].ord, bytes[3].ord)
   end
 
   def length
-    word16(bytes[4], bytes[5])
+    word16(bytes[4].ord, bytes[5].ord)
   end
 
   def checksum
-    word16(bytes[6], bytes[7])
+    word16(bytes[6].ord, bytes[7].ord)
   end
 
   def word16(a, b)
-    (a << 8) | b
+    (a << 8) | b  # Use bitwise OR to combine the bytes
   end
 
   def body
-    bytes[8, (length - 8)].pack('C*')
+     bytes[8, (length - 8)].pack('C*').force_encoding('UTF-8')
   end
 end
